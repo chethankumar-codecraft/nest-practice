c88116e13caf6598507c258d37f5f515e0bd3076
chethankumar-codecraft <chethan.kumar@codecraft.co.in>
2026-06-10 12:28:17 +0000

    wed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatController } from './cat/cat.controller';
 import { UserModule } from './user/user.module';
 import { CatModule } from './cat/cat.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [UserModule, CatModule],
   controllers: [AppController, CatController],
   providers: [AppService],
 })
-export class AppModule  { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(CatController);
+  }
+}
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,9 +1,15 @@
-import { Injectable, NestMiddleware, Req, Request, Response } from "@nestjs/common";
+import { Injectable, NestMiddleware, Next, Req, Request, Response } from "@nestjs/common";
+import { NestApplication } from "@nestjs/core";
+import { url } from "inspector";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    use(@Request() req, @Response() res) {
-        console.log(req)
+    use(req, res, next) {
+        console.log('METHOD: ', req.method);
+        console.log('URL: ', req.url);
+        console.log(`HEADERS: \n host:${req.headers.host} \n authorization:${req.headers.authorization}`);
+        console.log('BODY: ', req.body);
+        next();
     }
 
 }
